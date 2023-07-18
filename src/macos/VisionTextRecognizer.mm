5e8288baf085cdcfff59642fed34890cdfcaffda
Kaito Udagawa <umireon@gmail.com>
2023-07-18 14:38:44 +0900

    Fix

--- src/macos/VisionTextRecognizer.mm
+++ src/macos/VisionTextRecognizer.mm
@@ -1,12 +1,12 @@
 #import <Foundation/Foundation.h>
 #import <AVFoundation/AVFoundation.h>
+#import "VisionTextRecognizer.h"
 
 std::string recognizeText(const cv::Mat& imageBGRA)
 {
-    cv::Mat imageBGRA;
-
-    int imageWidth = imageBGR.cols;
-    int imageHeight = imageBGR.rows;
-
-    return std::string("aaaa");
+    CVPixelBufferRef pixelBuffer;
+    int width = imageBGRA.cols;
+    int height = imageBGRA.rows;
+    CVPixelBufferCreateWithBytes(kCFAllocatorDefault, width, height, kCVPixelFormatType_32BGRA, imageBGRA.data, width * 4, nil, nil, nil, &pixelBuffer);
+    return std::string("aaa") + std::to_string(imageBGRA.rows);
 }
