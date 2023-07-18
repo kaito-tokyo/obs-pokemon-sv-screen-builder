c784e2411176a3efd8e21e37242686e571f4a12d
Kaito Udagawa <umireon@gmail.com>
2023-07-18 23:44:07 +0900

    Fix

--- src/Base64.hpp
+++ src/Base64.hpp
@@ -3,36 +3,41 @@
 #include <string>
 
 namespace Base64 {
-    std::string encode(std::vector<unsigned char> input) {
-        const char table[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+std::string encode(std::vector<unsigned char> input)
+{
+	const char table[] =
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
-        char encoded[(input.size() + 2) / 3 * 4 + 1];
+	char encoded[(input.size() + 2) / 3 * 4 + 1];
 
-        size_t i = 0, j = 0;
-        for (; j < input.size() - 2; i += 4, j += 3) {
-            encoded[i] = table[input[j] >> 2];
-            encoded[i+1] = table[((input[j] << 4) & 0x30) | input[j+1] >> 4];
-            encoded[i+2] = table[((input[j+1] << 2) & 0x3C) | input[j+2] >> 6];
-            encoded[i+3] = table[input[j+2] & 0x3F];
-        }
+	size_t i = 0, j = 0;
+	for (; j < input.size() - 2; i += 4, j += 3) {
+		encoded[i] = table[input[j] >> 2];
+		encoded[i + 1] =
+			table[((input[j] << 4) & 0x30) | input[j + 1] >> 4];
+		encoded[i + 2] =
+			table[((input[j + 1] << 2) & 0x3C) | input[j + 2] >> 6];
+		encoded[i + 3] = table[input[j + 2] & 0x3F];
+	}
 
-        int pad = input.size() % 3;
-        if (pad == 0) {
-            encoded[i] = '\0';
-        } else if (pad == 1) {
-            encoded[i] = table[input[j] >> 2];
-            encoded[i+1] = table[(input[j] << 4) & 0x30];
-            encoded[i+2] = '=';
-            encoded[i+3] = '=';
-            encoded[i+4] = '\0';
-        } else if (pad == 2) {
-            encoded[i] = table[input[j] >> 2];
-            encoded[i+1] = table[((input[j] << 4) & 0x30) | input[j+1] >> 4];
-            encoded[i+2] = table[(input[j+1] << 2) & 0x3C];
-            encoded[i+3] = '=';
-            encoded[i+4] = '\0';
-        }
+	int pad = input.size() % 3;
+	if (pad == 0) {
+		encoded[i] = '\0';
+	} else if (pad == 1) {
+		encoded[i] = table[input[j] >> 2];
+		encoded[i + 1] = table[(input[j] << 4) & 0x30];
+		encoded[i + 2] = '=';
+		encoded[i + 3] = '=';
+		encoded[i + 4] = '\0';
+	} else if (pad == 2) {
+		encoded[i] = table[input[j] >> 2];
+		encoded[i + 1] =
+			table[((input[j] << 4) & 0x30) | input[j + 1] >> 4];
+		encoded[i + 2] = table[(input[j + 1] << 2) & 0x3C];
+		encoded[i + 3] = '=';
+		encoded[i + 4] = '\0';
+	}
 
-        return encoded;
-    }
+	return encoded;
+}
 }
--- src/screen-source.cpp
+++ src/screen-source.cpp
@@ -7,6 +7,7 @@
 #include <nlohmann/json.hpp>
 
 #include <obs-module.h>
+#include <obs-frontend-api.h>
 
 #include "EntityCropper.h"
 #include "SceneDetector.h"
@@ -446,6 +447,42 @@ static bool add_text_sources_to_list(void *param, obs_source_t *source)
 	return true;
 }
 
+static bool handleClickAddDefaultLayout(obs_properties_t *props,
+					obs_property_t *property, void *data)
+{
+	UNUSED_PARAMETER(props);
+	UNUSED_PARAMETER(property);
+	UNUSED_PARAMETER(data);
+
+	obs_source_t *scene_source = obs_frontend_get_current_scene();
+	obs_scene_t *scene = obs_scene_from_source(scene_source);
+
+	const char *opponentTeamSourceName =
+		obs_module_text("OpponentTeamSource");
+	obs_source_t *origOpponentTeamSource =
+		obs_get_source_by_name(opponentTeamSourceName);
+	if (origOpponentTeamSource) {
+		obs_source_release(origOpponentTeamSource);
+	} else {
+		obs_data_t *opponentTeamSettings = obs_data_create();
+		char *path = obs_module_file("browser/opponent-team.html");
+		obs_data_set_string(opponentTeamSettings, "local_file", path);
+		obs_data_set_int(opponentTeamSettings, "width", 104);
+		obs_data_set_int(opponentTeamSettings, "height", 664);
+		obs_data_set_bool(opponentTeamSettings, "is_local_file", true);
+		obs_source_t *opponentTeamSource = obs_source_create(
+			"browser_source", opponentTeamSourceName,
+			opponentTeamSettings, nullptr);
+		obs_scene_add(scene, opponentTeamSource);
+		obs_data_release(opponentTeamSettings);
+		obs_source_release(opponentTeamSource);
+		bfree(path);
+	}
+
+	obs_scene_release(scene);
+	return true;
+}
+
 static obs_properties_t *screen_properties(void *data)
 {
 	screen_context *context = reinterpret_cast<screen_context *>(data);
@@ -472,6 +509,12 @@ static obs_properties_t *screen_properties(void *data)
 				 prop_opponent_rank_source);
 	}
 
+	//obs_property_t *prop_add_browser_sources =
+	obs_properties_add_button(
+		props, "add_default_layout_button",
+		obs_module_text("AddDefaultLayoutDescription"),
+		&handleClickAddDefaultLayout);
+
 	return props;
 }
 
@@ -494,7 +537,8 @@ static void drawOpponentPokemons(screen_context *context)
 
 		std::vector<uchar> pngImage;
 		cv::imencode(".png", resizedBGRA, pngImage);
-		imageUrls[i] = "data:image/png;base64," + Base64::encode(pngImage);
+		imageUrls[i] =
+			"data:image/png;base64," + Base64::encode(pngImage);
 	}
 
 	nlohmann::json json{
