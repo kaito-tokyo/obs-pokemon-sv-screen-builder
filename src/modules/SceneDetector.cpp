fa02e6d55e6a5dd9cca205959c78bf97855d7cba
Kaito Udagawa <umireon@gmail.com>
2023-07-19 14:47:00 +0900

    Update SceneDetector.cpp

--- src/modules/SceneDetector.cpp
+++ src/modules/SceneDetector.cpp
@@ -80,7 +80,7 @@ bool SceneDetector::isOpponentRankShown(const cv::Mat &screenTextBinary) const
 	for (size_t i = 0; i < TEXT_TEMPLATES.size(); i++) {
 		const cv::Mat &resultImage = image ^ TEXT_TEMPLATES[i];
 		const auto actual = cv::sum(resultImage) / 255;
-		if (actual[0] < image.total() * 0.2) {
+		if (actual[0] < static_cast<double>(image.total()) * 0.2) {
 			return true;
 		}
 	}
