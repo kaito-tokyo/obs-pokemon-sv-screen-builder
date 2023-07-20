299b69baa3596da29512ca3483cb7ebc6e91f91d
Kaito Udagawa <umireon@gmail.com>
2023-07-20 23:21:47 +0900

    Update Package-Windows.ps1 (#61)

--- .github/scripts/Package-Windows.ps1
+++ .github/scripts/Package-Windows.ps1
@@ -79,6 +79,16 @@ function Package {
         Invoke-External iscc ${IsccFile} /O"${ProjectRoot}/release" /F"${OutputName}-Installer"
         Remove-Item -Path Package -Recurse
         Pop-Location -Stack BuildTemp
+
+        Log-Group "Archiving ${ProductName}..."
+        $CompressArgs = @{
+            Path = (Get-ChildItem -Path "${ProjectRoot}/release/${Configuration}" -Exclude "${OutputName}*.*")
+            CompressionLevel = 'Optimal'
+            DestinationPath = "${ProjectRoot}/release/${OutputName}.zip"
+            Verbose = ($Env:CI -ne $null)
+        }
+
+        Compress-Archive -Force @CompressArgs
     } else {
         Log-Group "Archiving ${ProductName}..."
         $CompressArgs = @{
