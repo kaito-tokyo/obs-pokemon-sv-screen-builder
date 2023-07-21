35b660d80ac23cf1589b2fd2a4bfa3a3a410f7e3
Kaito Udagawa <umireon@gmail.com>
2023-07-22 03:37:06 +0900

    WindowsでOCRを実行できるようにする (#71)
    * Add VC project
    
    * Renew
    
    * Fix
    
    * Fix
    
    * Update CMakeLists.txt
    
    * Lint

--- src/modules/TextRecognizer.h
+++ src/modules/TextRecognizer.h
@@ -5,6 +5,16 @@
 
 #ifdef __APPLE__
 std::string recognizeText(const cv::Mat &imageBGR);
+#elif _WIN32
+std::string recognizeTextImpl(const unsigned char *data, int width, int height);
+
+static std::string recognizeText(const cv::Mat &imageBGRA)
+{
+	cv::Mat padImage;
+	cv::copyMakeBorder(imageBGRA, padImage, 200, 200, 200, 200,
+			   cv::BORDER_CONSTANT, cv::Scalar(255));
+	return recognizeTextImpl(padImage.data, padImage.cols, padImage.rows);
+}
 #else
 static std::string recognizeText(const cv::Mat &imageBGRA)
 {
