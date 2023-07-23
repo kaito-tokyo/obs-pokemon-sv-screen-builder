731d2d32a9bc79a989090b298cb6202b89675ef6
Kaito Udagawa <umireon@gmail.com>
2023-07-23 15:02:18 +0900

    Add cbor

---
+++ data-generator/SceneDetector_lobbyRankShown.py
@@ -0,0 +1,29 @@
+import cv2
+from glob import glob
+import numpy as np
+from os import path
+from cbor2 import dump
+
+rect = [542, 894, 119, 37]
+threshold = 128
+data = []
+cols = []
+for file in glob('./assets/screenshots/SceneDetector/*.png'):
+    name = path.basename(file)
+    srcImg = cv2.imread(file)
+    destImg = srcImg[rect[1]:rect[1] + rect[3], rect[0]:rect[0] + rect[2]]
+    destImg = cv2.cvtColor(destImg, cv2.COLOR_BGR2GRAY)
+    retval, destImg = cv2.threshold(destImg, threshold, 255, cv2.THRESH_BINARY)
+    cv2.imwrite("./assets/SceneDetector/" + name, destImg)
+
+    cols.append(destImg.shape[1])
+    data.append(np.ravel(destImg).tolist())
+
+with open('data/config/SceneDetector_lobbyRankShown.cbor', 'wb') as fp:
+    dump({
+        "rect": rect,
+        "threshold": threshold,
+        "ratio": 0.2,
+        "cols": cols,
+        "data": data,
+    }, fp)
