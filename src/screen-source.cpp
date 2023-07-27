673fbb6edf3d544c8835a68a5da9020b43a76ed1
Kaito Udagawa <umireon@gmail.com>
2023-07-27 23:15:39 +0900

    Rename Automaton

--- src/screen-source.cpp
+++ src/screen-source.cpp
@@ -349,7 +349,7 @@ try {
 	cv::threshold(gameplay_gray, gameplay_binary, 200, 255,
 		      cv::THRESH_BINARY);
 
-	context->automaton(context->gameplay_bgra);
+	context->StateMachine(context->gameplay_bgra);
 
 	UNUSED_PARAMETER(seconds);
 } catch (const std::exception &e) {
