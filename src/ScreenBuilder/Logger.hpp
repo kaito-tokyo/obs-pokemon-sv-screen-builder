2c3e7c3b3a5eb376465bc997b2102c523ab66ba9
Kaito Udagawa <umireon@gmail.com>
2023-07-30 00:24:52 +0900

    Logger.hpp

---
+++ src/ScreenBuilder/Logger.hpp
@@ -0,0 +1,108 @@
+#pragma once
+
+#include <string>
+#include <filesystem>
+#include <fstream>
+
+#include <opencv2/opencv.hpp>
+
+class Logger {
+public:
+	std::filesystem::path basedir;
+
+	std::string getPrefix(void) const
+	{
+		std::ostringstream oss;
+		std::time_t now = std::time(nullptr);
+		std::tm *localTime = std::localtime(&now);
+		oss << std::put_time(localTime, "%Y%m%dT%H%M%S");
+		return oss.str();
+	}
+
+	void writeOpponentPokemonImage(const std::string &prefix, int index,
+				       const cv::Mat &image) const
+	{
+		if (basedir.empty()) {
+			return;
+		}
+		std::ostringstream oss;
+		oss << prefix << "-OpponentPokemon-" << index << ".png";
+		const std::string filename = oss.str();
+		writeImage(oss.str(), image);
+	}
+
+	void writeOpponentRankImage(const std::string &prefix,
+				    const cv::Mat &image) const
+	{
+		if (basedir.empty()) {
+			return;
+		}
+		std::ostringstream oss;
+		oss << prefix << "-OpponentRank.png";
+		const std::string filename = oss.str();
+		writeImage(oss.str(), image);
+	}
+
+	void writeMyRankImage(const std::string &prefix,
+			      const cv::Mat &image) const
+	{
+		if (basedir.empty()) {
+			return;
+		}
+		std::ostringstream oss;
+		oss << prefix << "-MyRank.png";
+		const std::string filename = oss.str();
+		writeImage(oss.str(), image);
+	}
+
+	void writeMyPokemonImage(const std::string &prefix, int index,
+				 const cv::Mat &image) const
+	{
+		if (basedir.empty()) {
+			return;
+		}
+		std::ostringstream oss;
+		oss << prefix << "-MyPokemon-" << index << ".png";
+		const std::string filename = oss.str();
+		writeImage(oss.str(), image);
+	}
+
+	void writeScreenshot(const std::string &prefix, const std::string &name,
+			     const cv::Mat &image) const
+	{
+		if (basedir.empty()) {
+			return;
+		}
+		std::ostringstream oss;
+		oss << prefix << "-" << name << ".png";
+		const std::string filename = oss.str();
+		writeImage(oss.str(), image);
+	}
+
+	void writeOpponentTeamText(
+		const std::string &prefix,
+		const std::vector<std::string> &pokemonNames) const
+	{
+		if (basedir.empty()) {
+			return;
+		}
+		std::filesystem::path p = basedir;
+		p /= prefix + "-OpponentTeam.txt";
+		std::ofstream ofs(p);
+		for (const std::string &pokemonName : pokemonNames) {
+			ofs << pokemonName << std::endl;
+		}
+	}
+
+private:
+	void writeImage(const std::string &filename, const cv::Mat &image) const
+	{
+		if (image.empty()) {
+			return;
+		}
+		std::filesystem::create_directories(basedir);
+		std::filesystem::path p(basedir);
+		p /= filename;
+		cv::imwrite(p.string<char>(), image);
+	}
+};
