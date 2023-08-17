7fb0c377c08bb7d64f5b0842c834fbb3e85927ac
Kaito Udagawa <umireon@gmail.com>
2023-08-17 21:38:28 +0900

    TextRecognizerをリファクタリング (#125)
    * Refactor
    
    * Fix

---
+++ src/TextRecognizer/TesseractTextRecognizer/TesseractTextRecognizer.cpp
@@ -0,0 +1,11 @@
+#include <string>
+
+#include <opencv2/opencv.hpp>
+
+#include <obs.h>
+
+std::string recognizeText(const cv::Mat &imageBGRA)
+{
+	UNUSED_PARAMETER(imageBGRA);
+	return std::string("N/A");
+}
--- src/TextRecognizer/TextRecognizer.h
+++ src/TextRecognizer/TextRecognizer.h
@@ -3,18 +3,8 @@
 #include <string>
 #include <opencv2/opencv.hpp>
 
-#ifdef __APPLE__
+#if defined(__APPLE__) || defined(_WIN32)
 std::string recognizeText(const cv::Mat &imageBGR);
-#elif _WIN32
-std::string recognizeTextImpl(const unsigned char *data, int width, int height);
-
-static std::string recognizeText(const cv::Mat &imageBGRA)
-{
-	cv::Mat padImage;
-	cv::copyMakeBorder(imageBGRA, padImage, 200, 200, 200, 200,
-			   cv::BORDER_CONSTANT, cv::Scalar(255));
-	return recognizeTextImpl(padImage.data, padImage.cols, padImage.rows);
-}
 #else
 static std::string recognizeText(const cv::Mat &imageBGRA)
 {
