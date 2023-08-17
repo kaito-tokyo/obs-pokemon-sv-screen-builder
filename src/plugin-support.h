47f2fec42c05372213dcdc9e6df4b711122a22e7
Kaito Udagawa <umireon@gmail.com>
2023-08-18 02:29:50 +0900

    Fix

--- src/plugin-support.h
+++ src/plugin-support.h
@@ -1,6 +1,6 @@
 /*
-Plugin Name
-Copyright (C) <Year> <Developer> <Email Address>
+obs-pokemon-sv-screen-builder
+Copyright (C) 2023 Kaito Udagawa umireon@gmail.com
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
