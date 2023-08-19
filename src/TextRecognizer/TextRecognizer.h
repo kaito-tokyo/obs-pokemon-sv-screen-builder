81721c81bfe51eb7b183a584f664597396e22798
Kaito Udagawa <umireon@gmail.com>
2023-08-20 03:40:55 +0900

    Update TextRecognizer.h

--- src/TextRecognizer/TextRecognizer.h
+++ src/TextRecognizer/TextRecognizer.h
@@ -5,5 +5,5 @@
 
 #include <opencv2/opencv.hpp>
 
-std::string recognizeText(const cv::Mat &imageBinary,
-			  std::function<void(std::string)> callback);
+void recognizeText(const cv::Mat &imageBinary,
+		   std::function<void(std::string)> callback);
