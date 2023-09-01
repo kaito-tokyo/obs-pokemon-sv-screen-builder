3153bb69c99f0683fc2f20759eb20516fe7ba707
Kaito Udagawa <umireon@gmail.com>
2023-09-02 05:38:23 +0900

    Update StateMachine.cpp

--- src/ScreenBuilder/StateMachine.cpp
+++ src/ScreenBuilder/StateMachine.cpp
@@ -97,7 +97,7 @@ ScreenState StateMachine::computeRankShown(GameplayScene scene)
 ScreenState StateMachine::computeEnteringSelectPokemon(void)
 {
 	bool canEnterToSelectPokemon = elapsedNsFromLastStateChange >
-				       1000000000;
+				       2000000000;
 	actionHandler.handleEnteringSelectPokemon(gameplayBGRA, gameplayBGR,
 						  canEnterToSelectPokemon,
 						  mySelectionMap, matchState);
